714011f57377c12158799157418e4f9493e0a030
Noah King <noahking123@yahoo.com>
2022-08-25 10:06:00 -0400

    commit tree f23f5a4e66f715893cec6687da64381412fdf9e7

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
 
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour : employee[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+
+}
+
+function createTimeInEvent(event) {
+   console.log('event',event)
+    let [date, hour] = event.toString().split(" ")
+    let eventObj = {
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date
+    };
+    
+    this.timeInEvents.push(eventObj)
+}
+
+function createTimeOutEvent(event) {
+    let [date, hour] = event.toString().split(" ")
+    let eventObj = {
+        type: 'TimeOut',
+        hour: (hour, 10),
+        date
+    };
+    
+    this.timeOutEvents.push(eventObj)
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date)
+    const timeOut = this.timeOutEvents.find(event => event.date === date)
+
+    return (timeOut.hour = timeIn.hour)/100
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
